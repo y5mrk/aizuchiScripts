76a51bb40f6e035a366da90486d103de6f728bf2
y5mrk <kzm326.ag@gmail.com>
2023-06-11 18:43:44 +0900

    マックで動かすスクリプト

---
+++ src/aizuchiForMac.py
@@ -0,0 +1,47 @@
+import urllib.request, urllib.error
+from bs4 import BeautifulSoup
+import subprocess
+import random
+import logging
+import time
+import speech_recognition as sr
+
+isPlaying = True
+
+def aizuchi():
+    aizuchi_list = ['うん', 'うんうん', 'へー', 'ふーん', 'うん、うん']
+    text = random.choice(aizuchi_list)
+    say(text)
+    return
+
+def interval():
+    sleepTime = random.uniform(1.5, 10)
+    print(sleepTime)
+    time.sleep(sleepTime)
+
+def say(text):
+    subprocess.call('say "%s"' % text, shell=True)
+
+def stop():
+    isPlaying = False
+    print(isPlaying)
+
+def listen():
+  r = sr.Recognizer()
+  with sr.Microphone(sample_rate=16_000) as source:
+      print("なにか話してください")
+      audio = r.listen(source)
+      print("音声を取得しました")
+  recognized_text = r.recognize_google(audio, language='ja-JP')
+  print(recognized_text)
+  aizuchi()
+
+if __name__ == "__main__":
+  while True:
+    if isPlaying:
+        listen()
+        # print('Conversation started!')
+        # interval()
+        # aizuchi()
+    else:
+        print('Press button to start conversation...')

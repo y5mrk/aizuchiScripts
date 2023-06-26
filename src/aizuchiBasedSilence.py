2602ab201deb0b3f9ea065ea37157aa454a42a5a
y5mrk <kzm326.ag@gmail.com>
2023-06-27 07:16:47 +0900

    1.0秒以上の沈黙を検知して相槌を打つ

--- src/aizuchiBasedSilence.py
+++ src/aizuchiBasedSilence.py
@@ -2,10 +2,11 @@
 
 # ライブラリの読込
 import pyaudio
-import wave
 import numpy as np
-from datetime import datetime
 import math
+import time
+import random
+import subprocess
 
 # 音データフォーマット
 chunk = 1024
@@ -15,7 +16,7 @@ RATE = 44100
 RECORD_SECONDS = 2
 
 # 閾値
-threshold = 65
+threshold = 35.0
 
 # 音の取込開始
 audio = pyaudio.PyAudio()
@@ -26,21 +27,53 @@ stream = audio.open(format = FORMAT,
     frames_per_buffer = chunk
 )
 
-cnt = 0
+isSpeaking = False
+speakingTime = 0
+speakingStart = 0
+silenceStart = 0
+silenceEnd = 0
+
+enableAizuchi = False
+
+def aizuchi():
+    aizuchi_list = ['うん', 'へー', 'ふーん']
+    text = random.choice(aizuchi_list)
+    say(text)
+    print("相槌: " + text)
+    return
+
+def say(text):
+    subprocess.call('say "%s"' % text, shell = True)
 
 while True:
   try:
-    # 音データの取得
-    data = stream.read(chunk)
-
+    data = stream.read(chunk, exception_on_overflow = False)
     x = np.frombuffer(data, dtype="int16")
     rms = x.max()
     db = 20 * math.log10(rms) if rms > 0.0 else -math.inf
-    print(f"音量：{format(db, '3.1f')}[dB]")
 
-    # 閾値以上の場合はファイルに保存
-    if rms > threshold:
-        print("声を検出")
+    current = time.perf_counter()
+    silenceTime = current - silenceStart 
+
+    if silenceTime > 1.0 and silenceStart - speakingStart > 0 and enableAizuchi:
+      print(f"沈黙時間：{ silenceTime }秒")
+      aizuchi()
+      enableAizuchi = False
+
+    if db > threshold:
+      if not isSpeaking:
+        isSpeaking = True
+        speakingStart = time.perf_counter()
+        print("音を検知")
+    else:
+      if isSpeaking:
+        silenceStart = time.perf_counter()
+        
+        # 0.2秒より短い音を検知した場合は、相槌を打たない
+        if silenceStart - speakingStart > 0.2:
+          print(f"しゃべってた時間：{ silenceStart - speakingStart }秒")
+          enableAizuchi = True
+        isSpeaking = False
 
   except KeyboardInterrupt: ## ctrl+c で終了
       break
